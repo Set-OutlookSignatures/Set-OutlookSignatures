5bfb3c7692c30f7b7d75e3d61b2f14b26d927319
GruberMarkus <markus.gruber@gruber.cc>
2022-02-07 14:30:40 +0100

    fix: Use another Windows API that does not require read access to CN=Computers

--- src/Set-OutlookSignatures.ps1
+++ src/Set-OutlookSignatures.ps1
@@ -689,7 +689,6 @@ function main {
             $objNT = $objTrans.GetType()
             $objNT.InvokeMember('Init', 'InvokeMethod', $Null, $objTrans, (3, $Null)) # 3 = ADS_NAME_INITTYPE_GC
             $objNT.InvokeMember('Set', 'InvokeMethod', $Null, $objTrans, (12, $(([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value))) # 12 = ADS_NAME_TYPE_SID_OR_SID_HISTORY_NAME
-            $objNT.InvokeMember('Get', 'InvokeMethod', $Null, $objTrans, 1) # 1 = ADS_NAME_TYPE_1779 (DistinguishedName)
             $y = ([ADSI]"LDAP://$(($objNT.InvokeMember('Get', 'InvokeMethod', $Null, $objTrans, 1) -split ',DC=')[1..999] -join '.')/RootDSE").rootDomainNamingContext -replace ('DC=', '') -replace (',', '.')
 
             if ($y -ne '') {
@@ -808,8 +807,13 @@ function main {
         if ($null -ne $TrustsToCheckForGroups[0]) {
             try {
                 if (-not $SimulateUser) {
-                    $Search.SearchRoot = "GC://$((([System.DirectoryServices.AccountManagement.UserPrincipal]::Current).DistinguishedName -split ',DC=')[1..999] -join '.')"
-                    $Search.Filter = "((distinguishedname=$(([System.DirectoryServices.AccountManagement.UserPrincipal]::Current).DistinguishedName)))"
+                    $objTrans = New-Object -ComObject 'NameTranslate'
+                    $objNT = $objTrans.GetType()
+                    $objNT.InvokeMember('Init', 'InvokeMethod', $Null, $objTrans, (3, $Null)) # 3 = ADS_NAME_INITTYPE_GC
+                    $objNT.InvokeMember('Set', 'InvokeMethod', $Null, $objTrans, (12, $(([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value))) # 12 = ADS_NAME_TYPE_SID_OR_SID_HISTORY_NAME
+                    $temp = $objNT.InvokeMember('Get', 'InvokeMethod', $Null, $objTrans, 1)
+                    $Search.SearchRoot = "GC://$(($temp -split ',DC=')[1..999] -join '.')"
+                    $Search.Filter = "(distinguishedname=$($temp))"
                     $ADPropsCurrentUser = $Search.FindOne().Properties
                 } else {
                     try {
